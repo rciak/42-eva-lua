ff45b2ced9097f69a965dc0299f5ff1af5666e07
Rene Angelo Ciak <reciak@c2r1p7.42vienna.com>
2025-06-03 20:04:02 +0200

    Ci1-ft_printf: Secure Copy

---
+++ Ci1-ft_printf-crtests/Ci1-ft_printf-crtests.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Ci1-ft_printf-crtests.h                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/23 17:47:41 by reciak            #+#    #+#             */
+/*   Updated: 2025/06/03 19:56:32 by reciak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include <signal.h>
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+#include "ft_printf.h"
