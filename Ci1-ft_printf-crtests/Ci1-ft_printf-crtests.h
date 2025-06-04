d3db4d22154871a42d41f97fd64f9ca510e01698
Rene Angelo Ciak <reciak@c2r1p1.42vienna.com>
2025-06-04 16:38:27 +0200

    Ci1-ft_printf: Zwischensicherung

--- Ci1-ft_printf-crtests/Ci1-ft_printf-crtests.h
+++ Ci1-ft_printf-crtests/Ci1-ft_printf-crtests.h
@@ -6,15 +6,20 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 17:47:41 by reciak            #+#    #+#             */
-/*   Updated: 2025/06/04 11:05:27 by reciak           ###   ########.fr       */
+/*   Updated: 2025/06/04 15:25:01 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-#include <signal.h>
-#include <criterion/criterion.h>
-#include <criterion/redirect.h>
+#ifndef CI1_FT_PRINTF_CRTESTS_H
+# define CI1_FT_PRINTF_CRTESTS_H
+# include <stddef.h>
+# include <signal.h>
+# include <criterion/criterion.h>
+# include <criterion/redirect.h>
+# include <criterion/parameterized.h>
 
-#include <stdio.h>
-#include <unistd.h>
-#include "ft_printf.h"
+# include <stdio.h>
+# include <unistd.h>
+# include "ft_printf.h"
+
+#endif
--- Ci1-ft_printf-crtests/ft_printf-tests.c
+++ Ci1-ft_printf-crtests/ft_printf-tests.c
@@ -6,23 +6,60 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 17:47:50 by reciak            #+#    #+#             */
-/*   Updated: 2025/06/04 12:16:42 by reciak           ###   ########.fr       */
+/*   Updated: 2025/06/04 16:38:00 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Ci1-ft_printf-crtests.h"
 
-Test(ft_printf, dummy_without_args)
+// 1. Only strings /////////////////////////////////////////////////////////////
+
+typedef struct s_params0
+{
+	const char	*str;
+}	t_params0;
+
+static void st_pre0(t_params0 *param)
 {
 	cr_redirect_stdout();
-	ft_printf();
+printf("%s", param->str);
 	fflush(stdout);
-	cr_assert_stdout_eq_str("Dummy\n");
 }
-Test(ft_printf, dummy_without_args2)
+
+ParameterizedTestParameters(ft_printf, only_string)
 {
-	cr_redirect_stdout();
-	ft_printf();
-	fflush(stdout);	
-	cr_assert_stdout_eq_str("Dummy");
+	static t_params0 params[] = 
+	{
+		{"A"}, {"bra"}, {"ka"}, {"da"}, {"bra!"}, 
+		{""}, {"cba%%"}, {"c%"}
+	};
+	size_t nb_params = sizeof (params) / sizeof (t_params0);
+	return cr_make_param_array(t_params0, params, nb_params);
 }
+//
+ParameterizedTest(t_params0 *param, ft_printf, only_string)
+{
+	st_pre0(param);
+	cr_expect_stdout_eq_str(param->str);
+}
+
+
+
+
+
+
+
+// Test(ft_printf, dummy_without_args)
+// {
+// 	cr_redirect_stdout();
+// 	ft_printf();
+// 	fflush(stdout);
+// 	cr_assert_stdout_eq_str("Dummy\n");
+// }
+// Test(ft_printf, dummy_without_args2)
+// {
+// 	cr_redirect_stdout();
+// 	ft_printf();
+// 	fflush(stdout);	
+// 	cr_assert_stdout_eq_str("Dummy");
+// }
