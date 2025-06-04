9f443d52881a6e862ff1634c7092db9d890cac1f
Rene Angelo Ciak <reciak@c2r1p1.42vienna.com>
2025-06-04 12:26:18 +0200

    Ci1-ft_printf: Trying out criterions check of output to stdout:
    Two tests work now

--- Ci1-ft_printf-crtests/ft_printf-tests.c
+++ Ci1-ft_printf-crtests/ft_printf-tests.c
@@ -1,21 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf_test.c                                   :+:      :+:    :+:   */
+/*   ft_printf-tests.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 17:47:50 by reciak            #+#    #+#             */
-/*   Updated: 2025/06/04 11:07:42 by reciak           ###   ########.fr       */
+/*   Updated: 2025/06/04 12:16:42 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Ci1-ft_printf-crtests.h"
 
-Test(ft_printf, without_args)
+Test(ft_printf, dummy_without_args)
 {
 	cr_redirect_stdout();
 	ft_printf();
 	fflush(stdout);
+	cr_assert_stdout_eq_str("Dummy\n");
+}
+Test(ft_printf, dummy_without_args2)
+{
+	cr_redirect_stdout();
+	ft_printf();
+	fflush(stdout);	
 	cr_assert_stdout_eq_str("Dummy");
 }
