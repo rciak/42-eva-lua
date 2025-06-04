652941c6857a70b813c7097071e7955c206d700e
Rene Angelo Ciak <reciak@c2r1p1.42vienna.com>
2025-06-04 18:43:11 +0200

    Ci1-ft_printf: After dev tidied up: Makefile and ft_printf-tests

--- Ci1-ft_printf-crtests/ft_printf-tests.c
+++ Ci1-ft_printf-crtests/ft_printf-tests.c
@@ -6,14 +6,19 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 17:47:50 by reciak            #+#    #+#             */
-/*   Updated: 2025/06/04 16:38:00 by reciak           ###   ########.fr       */
+/*   Updated: 2025/06/04 18:39:09 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Ci1-ft_printf-crtests.h"
 
-// 1. Only strings /////////////////////////////////////////////////////////////
+// -1. Reaction to NULL
+Test(ft_printf, NULL_instead_string)
+{
+	ft_printf(NULL);
+}
 
+// 0. Only strings /////////////////////////////////////////////////////////////
 typedef struct s_params0
 {
 	const char	*str;
@@ -21,45 +26,26 @@ typedef struct s_params0
 
 static void st_pre0(t_params0 *param)
 {
+	ft_printf(param->str);
 	cr_redirect_stdout();
-printf("%s", param->str);
+	ft_printf(param->str);
 	fflush(stdout);
 }
-
 ParameterizedTestParameters(ft_printf, only_string)
 {
 	static t_params0 params[] = 
 	{
-		{"A"}, {"bra"}, {"ka"}, {"da"}, {"bra!"}, 
-		{""}, {"cba%%"}, {"c%"}
+		{"A "}, {"bra "}, {"ka "}, {"da "}, {"bra! "}, 
+		{"\n"}, {""}, {"cba%%"}, {"c%"}
 	};
 	size_t nb_params = sizeof (params) / sizeof (t_params0);
 	return cr_make_param_array(t_params0, params, nb_params);
 }
-//
 ParameterizedTest(t_params0 *param, ft_printf, only_string)
 {
+	char expected[1024];
+
 	st_pre0(param);
-	cr_expect_stdout_eq_str(param->str);
+	sprintf(expected, param->str);
+	cr_expect_stdout_eq_str(expected);
 }
-
-
-
-
-
-
-
-// Test(ft_printf, dummy_without_args)
-// {
-// 	cr_redirect_stdout();
-// 	ft_printf();
-// 	fflush(stdout);
-// 	cr_assert_stdout_eq_str("Dummy\n");
-// }
-// Test(ft_printf, dummy_without_args2)
-// {
-// 	cr_redirect_stdout();
-// 	ft_printf();
-// 	fflush(stdout);	
-// 	cr_assert_stdout_eq_str("Dummy");
-// }
